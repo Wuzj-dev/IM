e721e7b9479c4420c5de35a40467fa1b98b70c13
Wuj <1500318810@qq.com>
2022-07-09 12:55:33 +0800

    初始化

--- Start/common/src/main/java/com/han/common/utils/GsonUtil.java
+++ Start/common/src/main/java/com/han/common/utils/GsonUtil.java
@@ -1,5 +1,8 @@
 package com.han.common.utils;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 /**
  * @ClassNAME GsonUtil
  * @Description Gson工具
@@ -7,4 +10,36 @@ package com.han.common.utils;
  * @Date 2022/7/9 12:11
  */
 public class GsonUtil {
+
+    private static final Gson gson = new GsonBuilder().create();
+
+    private final static Gson getGson() {
+        return gson;
+    }
+
+    private static final String EMPTY = StringUtil.EMPTY;
+
+    /**
+     * 对象转 String 类型
+     *
+     * @param obj the Obj
+     * @return the String
+     */
+    public static String toJson(Object obj) {
+        if (obj == null) {
+            return EMPTY;
+        }
+        return getGson().toJson(obj);
+    }
+
+    /**
+     * String 类型 转 对象
+     *
+     * @param jsonStr the jsonStr
+     * @param tClass  the tClass
+     * @return the T
+     */
+    public static <T> T fromJson(String jsonStr, Class<T> tClass) {
+        return getGson().fromJson(jsonStr, tClass);
+    }
 }
---
+++ Start/common/src/main/java/com/han/common/utils/StringUtil.java
@@ -0,0 +1,12 @@
+package com.han.common.utils;
+
+/**
+ * @ClassNAME StringUtil
+ * @Description String 工具
+ * @Author wuj
+ * @Date 2022/7/9 12:50
+ */
+public class StringUtil {
+
+    public static final String EMPTY = "";
+}
